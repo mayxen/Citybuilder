7162e85e43f56bf0c0d27abcbc527e54d831854b
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2018-11-02 11:46:08 +0000

    Initial commit

---
+++ Assets/proyect/Scripts/Building.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+    public int id; //id the type of building
+    public int cost; //cost of the building
+    public string buildingName; //name of the building
+}
