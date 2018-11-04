42e18155f9abf6e9ba7cd9ec62762d699e6395ca
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2018-11-04 10:19:03 +0000

    logica
    Ya termine la lógica del juego para ir creciendo

---
+++ Assets/proyect/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    Vector3 mouseOriginPoint;
+    Vector3 offset;
+    bool dragging;
+
+
+    private void LateUpdate()
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * (Camera.main.orthographicSize*.1f),2.5f,50f);//change size of ortografic camera to do a zooming with the scroll
+
+        if (Input.GetMouseButton(2))//button 2 is the scroll 
+        {
+            offset = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.transform.position); //it take the mouse position and less it to the camera position
+            if (!dragging)
+            {
+                dragging = true;
+                mouseOriginPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition); //here we get the origin point of the mouse 
+            }
+        }
+        else
+        {
+            dragging = false;
+            
+        }
+        if (dragging)
+            Camera.main.transform.position = mouseOriginPoint - offset;
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/proyect/Scripts/City.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour {
+    
+    public int Cash { get; set; }
+    public int Day { get; set; }
+    public float Food { get; set; }
+    public float PopulationCurrent { get; set; }
+    public float PopulationCeiling { get; set; }
+    public float JobsCurrent { get; set; }
+    public float JobsCeiling { get; set; }
+
+    public int[] buildingCounts = new int [3];
+    // Use this for initialization
+    void Start () {
+        Cash = 10000;
+        Food = 5;
+        JobsCeiling = 10;
+
+	}
+
+    public void EndTurn()
+    {
+        Day++;
+        Debug.Log("heeey");
+        CalculatePopulation();
+        CalculateJobs();
+        CalculateCash();
+        CalculateFood();
+        Debug.LogFormat("Jobs: {0}/{1},Cash: {2},pop:{3}/{4},Food:{5}",JobsCurrent,JobsCeiling,Cash,PopulationCurrent,PopulationCeiling,Food);
+        
+    }
+    void CalculateJobs()
+    {
+        JobsCeiling = buildingCounts[2] * 10; //por cada fabrica 10 
+        JobsCurrent = Mathf.Min((int)PopulationCurrent ,JobsCeiling); //coge el minimo de los 2
+
+    }
+
+    void CalculateCash()
+    {
+        Cash += (int)JobsCurrent * 2;
+    }
+
+    void CalculateFood()
+    {
+        Food += buildingCounts[1] * 4f;
+    }
+
+    void CalculatePopulation()
+    {
+        PopulationCeiling = buildingCounts[0] * 5;
+        if(Food >= PopulationCurrent && PopulationCurrent < PopulationCeiling)
+        {
+            Food -= PopulationCurrent *.5f;//cada 0.5 food alimenta a 1 
+            PopulationCurrent = Mathf.Min(PopulationCurrent += Food * .5f,PopulationCeiling);
+        }
+        else if (Food < PopulationCurrent)
+        {
+            PopulationCurrent -= (PopulationCurrent - Food)*.5f;
+        }
+    }
+}
